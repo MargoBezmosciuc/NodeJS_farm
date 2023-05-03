2eea2d161e2816bc760c4b822b49a5ded43a0867
margo.bezmosciuc <margo.bezmosciuc@gmail.com>
2023-05-03 14:44:38 +0200

    Project in progress

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 /* const hello = "Hello World";
 console.log(hello);
  */
@@ -28,9 +29,27 @@ console.log("Will read the file");
  */
 
 ////////////////////////////// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  //   console.log(req);
-  res.end("Hello from the server Margo  create me");
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("Hello from Overview");
+  } else if (pathName === "/product") {
+    res.end("This is the Product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text.html",
+      "my-own-header": "Hello- World",
+    });
+    res.end("<h1>Page not found! </h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
