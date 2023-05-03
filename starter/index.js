3f52df86de601f9ab39fd7b1ec014e2d40f9ce7d
margo.bezmosciuc <margo.bezmosciuc@gmail.com>
2023-05-03 20:00:05 +0200

    Project Done

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,10 @@
+//core modules
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+//
+const replaceTemplate = require("./modules/replaceTemplate");
+//////////////////////////////////
 /* const hello = "Hello World";
 console.log(hello);
  */
@@ -29,19 +33,6 @@ console.log("Will read the file");
  */
 
 ////////////////////////////// SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/dev-data/templates/template-overview.html`,
@@ -94,6 +85,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(5000, "127.0.0.1", () => {
   console.log("Listening to request on port 8000");
 });
